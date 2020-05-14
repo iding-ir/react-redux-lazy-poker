7f15ca0e83d04b3bfcf7382ca6bcb9ecab789523
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-14 23:23:51 +0430

    Add app layout

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,19 @@ import "./app.css";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="app-container">
+        <header className="app-header"></header>
+
+        <div className="app-controls"></div>
+
+        <div className="app-ranking"></div>
+
+        <div className="app-deck"></div>
+
+        <div className="app-players"></div>
+      </div>
+    );
   }
 }
 
