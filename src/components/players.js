89e1486ffd5bb5c49490164ddc05206aaccdf988
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-15 19:56:45 +0430

    Refactor Players component

--- src/components/players.js
+++ src/components/players.js
@@ -1,35 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./players.css";
 import Player from "./player";
 
-class Players extends Component {
-  state = {};
-  render() {
-    let { players, addPlayer, removePlayer } = this.props;
+const Players = (props) => {
+  let { players, addPlayer, removePlayer } = props;
 
-    return (
-      <div className="players">
-        <div className="players-container">
-          {players.map((player, key) => {
-            return (
-              <Player key={key} player={player} removePlayer={removePlayer} />
-            );
-          })}
-        </div>
-
-        <div className="players-add">
-          <button
-            id="players-add"
-            className="waves-effect waves-light btn-large btn-floating pink"
-            onClick={addPlayer}
-          >
-            <i className="material-icons left">add</i>
-          </button>
-        </div>
+  return (
+    <div className="players">
+      <div className="players-container">
+        {players.map((player, key) => {
+          return (
+            <Player key={key} player={player} removePlayer={removePlayer} />
+          );
+        })}
       </div>
-    );
-  }
-}
+
+      <div className="players-add">
+        <button
+          id="players-add"
+          className="waves-effect waves-light btn-large btn-floating pink"
+          onClick={addPlayer}
+        >
+          <i className="material-icons left">add</i>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Players;
