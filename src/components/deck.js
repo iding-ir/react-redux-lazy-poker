6b8a234c1bd17b02a5662bf40c530063069376db
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-16 13:41:29 +0430

    Make it show back of cards

--- src/components/deck.js
+++ src/components/deck.js
@@ -2,14 +2,19 @@ import React from "react";
 
 import "./deck.css";
 import Card from "./card";
+import { defaultCard } from "../constants/game";
 
 const Deck = (props) => {
-  const { cards } = props;
+  let { table } = props.cards;
+
+  while (table.length < 5) {
+    table = [...table, defaultCard];
+  }
 
   return (
     <div className="deck">
       <div className="deck-cards">
-        {cards.table.map((card, index) => {
+        {table.map((card, index) => {
           return <Card key={index} card={card} />;
         })}
       </div>
