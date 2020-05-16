b99856b6364018401073705d6afd6b1f4532d5e6
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-17 00:32:23 +0430

    Add check function for flush

--- src/configs/index.js
+++ src/configs/index.js
@@ -5,6 +5,12 @@ export const NUMBER_OF_CARDS_FOR_FLOP = 3;
 export const NUMBER_OF_CARDS_FOR_TURN = 1;
 export const NUMBER_OF_CARDS_FOR_RIVER = 1;
 
+export const FLUSH_CONDITION = 5;
+export const STRAIGHT_CONDITION = 5;
+export const PAIR_CONDITION = 2;
+export const THREE_OF_A_KIND_CONDITION = 3;
+export const FOUR_OF_A_KIND_CONDITION = 4;
+
 export const AUTOPLAY_DELAY_MS = 2000;
 
 export const RANDOM_PLAYER_NAMES = ["Koshka", "Gulbi", "Remuss"];
--- src/utils/poker-evaluator.js
+++ src/utils/poker-evaluator.js
@@ -1,7 +1,8 @@
-import { SUITS } from "../constants/game";
+import { SUITS, RANKS } from "../constants/game";
+import { FLUSH_CONDITION } from "../configs";
 
 export default (final) => {
-  console.log(groupBySuit(final));
+  console.log(checkFlush(final));
 };
 
 function groupBySuit(cards) {
@@ -9,5 +10,16 @@ function groupBySuit(cards) {
     (total, suit) =>
       (total = [...total, cards.filter((card) => card.suit === suit)]),
     []
+  );
+}
+
+function orderByRanks(cards) {
+  return cards.sort((a, b) => RANKS.indexOf(a.rank) - RANKS.indexOf(b.rank));
+}
+
+function checkFlush(cards) {
+  return groupBySuit(cards).reduce(
+    (total, group) => total || group.length >= FLUSH_CONDITION,
+    false
   );
 }
