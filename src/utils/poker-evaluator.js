55f83a6df58d4a958ebfc32daac063ab91dedb45
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-17 17:09:43 +0430

    Add check straight function

--- src/utils/poker-evaluator.js
+++ src/utils/poker-evaluator.js
@@ -1,14 +1,22 @@
 import { SUITS, RANKS } from "../constants/game";
-import { FLUSH_CONDITION } from "../configs";
+import { FLUSH_CONDITION, STRAIGHT_CONDITION } from "../configs";
 
 export default (final) => {
-  console.log(checkFlush(final));
+  checkStraight(final);
 };
 
 function groupBySuit(cards) {
   return SUITS.reduce(
     (total, suit) =>
       (total = [...total, cards.filter((card) => card.suit === suit)]),
+    []
+  );
+}
+
+function groupByRank(cards) {
+  return RANKS.reduce(
+    (total, rank) =>
+      (total = [...total, cards.filter((card) => card.rank === rank)]),
     []
   );
 }
@@ -22,4 +30,21 @@ function checkFlush(cards) {
     (total, group) => total || group.length >= FLUSH_CONDITION,
     false
   );
+}
+
+function checkStraight(cards) {
+  const ranks = Object.values(cards).map((card) => card.rank);
+  let isStraight = false;
+
+  for (let i = 0; i <= RANKS.length - STRAIGHT_CONDITION + 1; i++) {
+    const straight = [...RANKS, RANKS[0]].slice(i, i + STRAIGHT_CONDITION);
+
+    if (straight.every((item) => ranks.includes(item))) {
+      isStraight = true;
+
+      break;
+    }
+  }
+
+  return isStraight;
 }
